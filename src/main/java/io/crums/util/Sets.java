c3fad0a20457d0ec70dfc346a62b242dbfb5e12a
gnahraf <farhang@gmail.com>
2020-11-29 18:15:42 -0700

    SortedSet utilities

--- src/main/java/io/crums/util/Sets.java
+++ src/main/java/io/crums/util/Sets.java
@@ -6,24 +6,111 @@ package io.crums.util;
 
 import java.util.AbstractSet;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
+import java.util.SortedSet;
 
 /**
- * 
+ * Utility methods for working sets. Mostly read-only, immutable idioms.
  */
 public class Sets {
 
   private Sets() {  }
   
   
-  
+  /**
+   * Returns a mapped view of the given <tt>source</tt> set. The elements of the
+   * set are lazily constucted.
+   * 
+   * @param <U> the source type
+   * @param <V> the target (return) type
+   * 
+   * @param source the source set (unmodified)
+   * @param iso isomorphism defining reversible mapping
+   * @return
+   */
   public static <U, V> Set<V> map(Set<U> source, Isomorphism<U, V> iso) {
     return new SetView<>(source, iso);
   }
   
   
+  /**
+   * Determines whether the given two sets intersect (have one or more common elements).
+   */
+  public static <T> boolean intersect(SortedSet<T> a, SortedSet<T> b) {
+    return firstIntersection(a, b) != null;
+  }
+  
+  
+  /**
+   * Returns the first element contained in both sets or <tt>null</tt>, if no such
+   * element is found.
+   */
+  @SuppressWarnings("unchecked")
+  public static <T> T firstIntersection(SortedSet<T> a, SortedSet<T> b) {
+    Objects.requireNonNull(a, "null a");
+    Objects.requireNonNull(b, "null b");
+    Comparator<? super T> comparator = a.comparator();
+    if (!Objects.equals(comparator, b.comparator()))
+      throw new IllegalArgumentException("comparators do not match: " + a + " , " + b.comparator());
+    
+    if (a.isEmpty() || b.isEmpty())
+      return null;
+    
+    while (!a.isEmpty() && !b.isEmpty()) {
+      
+      T firstA = a.first();
+      T firstB = b.first();
+      
+      int comp = comparator == null ?
+          ((Comparable<T>) firstA).compareTo(firstB) :
+            comparator.compare(firstA, firstB);
+      
+      if (comp < 0)
+        // firstA < firstB
+        a = a.tailSet(firstB);
+      else if (comp > 0)
+        // firstB < firstA
+        b = b.tailSet(firstA);
+      else
+        return firstA;
+    }
+    return null;
+  }
+  
+
+  /**
+   * Returns a <tt>SortedSet</tt> view of the given ordered list. The returned object has undefined behavior
+   * if the given list contains out-of-order elements.
+   * 
+   * @param orderedList the backing ordered list
+   * 
+   * @return non-null
+   */
+  public static <T extends Comparable<T>> SortedSet<T> sortedSetView(List<T> orderedList) {
+    return sortedSetView(orderedList, null);
+  }
+  
+  /**
+   * Returns a <tt>SortedSet</tt> view of the given ordered list. The returned object has undefined behavior
+   * if the given list contains out-of-order elements, or if the <tt>comparator</tt> is <tt>null</tt> and the
+   * elements are not mutually comparable.
+   * 
+   * @param <T> the type (implements <tt>Comparable&lt;T&gt;</tt> if <tt>comparator</tt> is <tt>null</tt>)
+   * 
+   * @param orderedList the backing ordered list
+   * @param comparator defines the ordering; if <tt>null</tt>, then <tt>&lt;T&gt;</tt> implements
+   *                   <tt>Comparable&lt;T&gt;</tt>
+   * @return non-null
+   */
+  public static <T> SortedSet<T> sortedSetView(List<T> orderedList, Comparator<T> comparator) {
+    Objects.requireNonNull(orderedList, "null orderedList");
+    return orderedList.isEmpty() ? Collections.emptySortedSet() : new SetViewList<>(orderedList, comparator);
+  }
   
   
   
@@ -75,5 +162,137 @@ public class Sets {
     }
     
   }
+  
+  
+  protected static class SetViewList<T> extends AbstractSet<T> implements SortedSet<T> {
+    
+    private final List<T> elements;
+    private final Comparator<T> comparator;
+    
+    protected SetViewList(List<T> orderedList, Comparator<T> comparator) {
+      this.elements = Objects.requireNonNull(orderedList, "null orderedList");
+      this.comparator = comparator;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T[] toArray(Object[] a) {
+      return (T[]) elements.toArray(a);
+    }
+
+    @Override
+    public boolean add(Object e) throws UnsupportedOperationException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+      return elements.containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) throws UnsupportedOperationException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) throws UnsupportedOperationException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) throws UnsupportedOperationException {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Comparator<T> comparator() {
+      return comparator;
+    }
+
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+      int size = elements.size();
+      int startIndex = binarySearch(fromElement);
+      if (startIndex < 0)
+        startIndex = -1 - startIndex;
+      
+      if (startIndex == size)
+        return Collections.emptySortedSet();
+      
+      int endIndex = binarySearch(toElement);
+      if (endIndex < 0)
+        endIndex = -1 - endIndex;
+      
+      if (endIndex <= startIndex)
+        return Collections.emptySortedSet();
+      
+      if (startIndex == 0 && endIndex == size)
+        return this;
+      
+      return new SetViewList<>(elements.subList(startIndex, endIndex), comparator);
+    }
+
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+      int index = binarySearch(toElement);
+      if (index < 0)
+        index = -1 - index;
+      int size = elements.size();
+      if (index == size)
+        return this;
+      
+      return index == 0 ?
+          Collections.emptySortedSet() :
+            new SetViewList<>(elements.subList(0, index), comparator);
+    }
+    
+
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+      int index = binarySearch(fromElement);
+      if (index < 0)
+        index = -1 - index;
+      if (index == 0)
+        return this;
+      
+      int size = elements.size();
+      
+      return index == size ?
+          Collections.emptySortedSet() :
+            new SetViewList<>(elements.subList(index, size), comparator);
+    }
+
+    @Override
+    public T first() {
+      return elements.isEmpty() ? null : elements.get(0);
+    }
+
+    @Override
+    public T last() {
+      int size = elements.size();
+      return size == 0 ? null : elements.get(size - 1);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+      return elements.iterator();
+    }
+
+    @Override
+    public int size() {
+      return elements.size();
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    private int binarySearch(T element) {
+      return comparator == null ?
+          Collections.binarySearch((List<? extends Comparable<T>>) elements, element) :
+            Collections.binarySearch(elements, element, comparator);
+      }
+    
+  }
+  
 
 }
---
+++ src/test/java/io/crums/util/SetsTest.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2020 Babak Farhang
+ */
+package io.crums.util;
+
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class SetsTest {
+  
+  
+  @Test
+  public void testFirstIntersection_00() {
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = { 1, 3, 9, };
+    int expected = 1;
+    
+    testFirstIntersection(a, b, expected);
+  }
+  
+  
+  @Test
+  public void testFirstIntersection_01(){
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = { 0, 2, 11, };
+//    int expected = 1;
+    
+    testFirstIntersection(a, b, null);
+  }
+
+  
+  @Test
+  public void testFirstIntersection_02() {
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = { 0, 3, 9, };
+    int expected = 3;
+    
+    testFirstIntersection(a, b, expected);
+  }
+
+  
+  @Test
+  public void testFirstIntersection_03() {
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = { 0, 9, };
+    int expected = 9;
+    
+    testFirstIntersection(a, b, expected);
+  }
+
+  
+  @Test
+  public void testFirstIntersection_04() {
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = { 0, 9, };
+    int expected = 9;
+    
+    testFirstIntersection(a, b, expected);
+  }
+
+  
+  @Test
+  public void testFirstIntersection_05() {
+    Integer[] a = { 1, 3, 9, };
+    Integer[] b = {  };
+//    int expected = 9;
+    
+    testFirstIntersection(a, b, null);
+  }
+
+  
+  @Test
+  public void testFirstIntersection_06() {
+    Integer[] a = {  };
+    Integer[] b = {  };
+//    int expected = 9;
+    
+    testFirstIntersection(a, b, null);
+  }
+  
+  
+  private void testFirstIntersection(Integer[] a, Integer[] b, Integer expected) {
+    testFirstIntersection(Arrays.asList(a), Arrays.asList(b), expected);
+  }
+  
+  private void testFirstIntersection(List<Integer> a, List<Integer> b, Integer expected) {
+    SortedSet<Integer> setA = Collections.unmodifiableSortedSet(new TreeSet<>(a));
+    SortedSet<Integer> setB = Collections.unmodifiableSortedSet(new TreeSet<>(b));
+    assertEquals(expected, Sets.firstIntersection(setA, setB));
+    assertEquals(expected, Sets.firstIntersection(setB, setA));
+  }
+  
+
+
+  @Test
+  public void testSetViewList_Empty() {
+    Integer[] ordered = { };
+    testSetViewList(ordered);
+  }
+  
+  
+  @Test
+  public void testSetViewList_Singleton() {
+    Integer[] ordered = { 9, };
+    testSetViewList(ordered);
+  }
+  
+  
+  @Test
+  public void testSetViewList_Pair() {
+    Integer[] ordered = { -9, 9, };
+    testSetViewList(ordered);
+  }
+
+  @Test
+  public void testSetViewList() {
+    Integer[] ordered = { 0, 5, 10, 15, 20, };
+    testSetViewList(ordered);
+  }
+  
+  
+  
+  private void testSetViewList(Integer[] ordered) {
+    List<Integer> expected = Arrays.asList(ordered);
+    
+    SortedSet<Integer> actual = Sets.sortedSetView(expected);
+    assertSet(expected, actual);
+  }
+  
+  
+  
+  private void assertSet(List<Integer> expected, SortedSet<Integer> actual) {
+    final int size = expected.size();
+    assertEquals(size, actual.size());
+    for (int index = 0; index < size; ++index) {
+      Integer value = expected.get(index);
+      
+      SortedSet<Integer> head = actual.headSet(value);
+      assertEquals(index, head.size());
+      List<Integer> expectedHead = expected.subList(0, index);
+      assertElements(expectedHead, head);
+      
+      // assume gaps
+      head = actual.headSet(value - 1);
+      assertElements(expectedHead, head);
+      
+      SortedSet<Integer> tail = actual.tailSet(value);
+      assertEquals(size - index, tail.size());
+      
+      List<Integer> expectedTail = expected.subList(index, size);
+      assertElements(expectedTail, tail);
+      
+      tail = actual.tailSet(value - 1);
+      assertElements(expectedTail, tail);
+    }
+  }
+  
+  
+  private void assertElements(List<Integer> expected, SortedSet<Integer> actual) {
+    Iterator<Integer> iExpected = expected.iterator();
+    Iterator<Integer> iActual = actual.iterator();
+    while (iExpected.hasNext())
+      assertEquals(iExpected.next(), iActual.next());
+    
+    assertFalse(iActual.hasNext());
+  }
+  
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
